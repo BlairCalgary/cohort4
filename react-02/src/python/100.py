a3f2d4d5c823f30a6b2eada61ee40eec25dafaf2
BlairCalgary <59458311+BlairCalgary@users.noreply.github.com>
2020-06-08 08:37:05 -0600

    converted to function

--- react-02/src/python/100.py
+++ react-02/src/python/100.py
@@ -1,12 +1,15 @@
 # User inputs 2 numbers to be added. Program returns the answer with a classification base on size
 first = int(input('Enter first number to add: '))
 second = int(input('Enter second number to add: '))
-answer = first + second
-print(f'{first} + {second} = {answer}.')
-if answer >= 20:
-    size = 'large'
-elif answer >= 10:
-    size = 'medium'
-else:
-    size = 'small'
-print(f'{answer} is a {size} number.')
+def add(first, second):
+    answer = first + second
+    if answer >= 20:
+        size = 'large'
+    elif answer >= 10:
+        size = 'medium'
+    else:
+        size = 'small'
+    print(f'{first} + {second} = {answer}.')
+    print(f'{answer} is a {size} number.')
+
+add(first, second)
