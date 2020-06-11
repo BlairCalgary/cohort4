f6cc036ea09d854d218a0b9a99b64382036c0224
BlairCalgary <59458311+BlairCalgary@users.noreply.github.com>
2020-06-10 21:18:02 -0600

    excel to dictionary

--- react-02/src/python/excel.py
+++ react-02/src/python/excel.py
@@ -1,57 +1,42 @@
 from openpyxl import load_workbook
 
-# Declare workbook object
-wb_obj = load_workbook('excel.xlsx')
-# Get sheetnames from workbook
-sheetnames = wb_obj.sheetnames
+def breakIntoDict():
+    wb = load_workbook('excel.xlsx')
+    builtDict = {}
+    for sheet in wb.sheetnames:
+        builtDict[sheet] = {}
+        rowCount = 1
+        for row in wb[sheet].iter_rows(min_row=2,min_col=1):
+            if row[0].value is not None:
+                col = 0
+                builtDict[sheet][rowCount] = {}
+                for cell in row:
+                    builtDict[sheet][rowCount][wb[sheet][1][col].value] = cell.value
+                    col += 1
+            rowCount += 1
+    # print(builtDict)
+    return builtDict
+xlDict = breakIntoDict()
 
-# for a in sheetnames: # Loop through sheets
-#     print('Sheetname:',a) # Print the sheetname
-#     ws_obj = wb_obj[a] # Load active sheet
-#     max_row = ws_obj.max_row # Max rows
-#     max_col = ws_obj.max_column # Max columns
-#     for b in range(1, max_col + 1):
-#         cell_obj = ws_obj.cell(row = 1, column = b )
-#         print(cell_obj.value)
-#         for c in range(2, max_row +1):
-#             cell_obj = ws_obj.cell(row = c, column = 1)
-#             print(cell_obj.value)
+# This block of code creates a list of existing invoice
+# number to display to the user
+invoices = xlDict['invoices']
+invoiceList = []
+for invoice in invoices:
+    invoiceList.append(invoices[invoice]['Invoice'])
+print(invoiceList)
 
-wb_obj.active = 0
-ws_obj = wb_obj.active            
-# print(ws_obj)
+# This code gets a valid invoice number from the user
+invoiceReq = 0
+while invoiceReq not in invoiceList:
+    invoiceReq = int(input('Enter existing invoice:'))
 
-custDict = {}
-max_row = ws_obj.max_row
-# print('max_row:',max_row)
-
-nameVar = (ws_obj[1][1].value)
-
-for i in range(2,max_row+1):
-    rowPair = {}
-    rowPair[nameVar] = ws_obj[i][1].value
-    custDict[ws_obj[i][0].value] = rowPair
-    # print(rowPair)
-    # print(ws_obj[i][0].value,nameVar,ws_obj[i][1].value)
-print(custDict[1]['Name'])
-# {
-#   "1":{"Name":"Blair"},
-#   "2":{"Name":"Greg"},
-#   "3":{"Name":"Larry"},
-#   "4":{"Name":"Roman"},
-# }
-def test_cust():
-    testName = custDict[2]['Name']
-    assert(testName)=='Greg'
-
-
-# custDict = {}
-# max_col = ws_obj.max_column
-# print('max_column:',max_col)
-# for row in ws_obj.values:
-#     for x in range(0,max_col):
-#         for value in row:
-#             print(value)
-
-
-
+# Invoice requires:
+# Invoice number (from invoice dictionary)
+# customer ID (from invoice dictionary)
+# customer name (from customers dictionary)
+# product ID (from invoice_line_items)
+# product qty (from invoice_line_items)
+# product name (from product dictionary)
+# unit price (from product dictionary)
+# total price (calculated)
